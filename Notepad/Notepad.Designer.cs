3571319937531643cd30ad364de0c10fe9596e58
kiheyunkim <kiheyunKim@gmail.com>
2019-10-08 13:42:22 +0900

    초기 커밋.

---
+++ Notepad/Notepad.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Calculator
+{
+    partial class Notepad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.편집ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Copy = new System.Windows.Forms.ToolStripMenuItem();
+            this.Cut = new System.Windows.Forms.ToolStripMenuItem();
+            this.Paste = new System.Windows.Forms.ToolStripMenuItem();
+            this.설정ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FontSetting = new System.Windows.Forms.ToolStripMenuItem();
+            this.Info = new System.Windows.Forms.ToolStripMenuItem();
+            this.Infomation = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TextBox.Location = new System.Drawing.Point(0, 24);
+            this.TextBox.Margin = new System.Windows.Forms.Padding(0);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(800, 426);
+            this.TextBox.TabIndex = 0;
+            this.TextBox.Text = "";
+            this.TextBox.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.편집ToolStripMenuItem,
+            this.설정ToolStripMenuItem,
+            this.Info});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewFile,
+            this.FileOpen,
+            this.Save,
+            this.SaveAs,
+            this.Exit});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // NewFile
+            // 
+            this.NewFile.Name = "NewFile";
+            this.NewFile.Size = new System.Drawing.Size(178, 22);
+            this.NewFile.Text = "새로 만들기";
+            this.NewFile.Click += new System.EventHandler(this.NewFile_Click);
+            // 
+            // FileOpen
+            // 
+            this.FileOpen.Name = "FileOpen";
+            this.FileOpen.Size = new System.Drawing.Size(178, 22);
+            this.FileOpen.Text = "열기";
+            this.FileOpen.Click += new System.EventHandler(this.FileOpen_Click);
+            // 
+            // Save
+            // 
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(178, 22);
+            this.Save.Text = "저장";
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // SaveAs
+            // 
+            this.SaveAs.Name = "SaveAs";
+            this.SaveAs.Size = new System.Drawing.Size(178, 22);
+            this.SaveAs.Text = "다른 이름으로 저장";
+            this.SaveAs.Click += new System.EventHandler(this.SaveAs_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(178, 22);
+            this.Exit.Text = "종료";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // 편집ToolStripMenuItem
+            // 
+            this.편집ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Copy,
+            this.Cut,
+            this.Paste});
+            this.편집ToolStripMenuItem.Name = "편집ToolStripMenuItem";
+            this.편집ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.편집ToolStripMenuItem.Text = "편집";
+            // 
+            // Copy
+            // 
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(180, 22);
+            this.Copy.Text = "복사";
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // Cut
+            // 
+            this.Cut.Name = "Cut";
+            this.Cut.Size = new System.Drawing.Size(180, 22);
+            this.Cut.Text = "잘라내기";
+            this.Cut.Click += new System.EventHandler(this.Cut_Click);
+            // 
+            // Paste
+            // 
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(180, 22);
+            this.Paste.Text = "붙여넣기";
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // 설정ToolStripMenuItem
+            // 
+            this.설정ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FontSetting});
+            this.설정ToolStripMenuItem.Name = "설정ToolStripMenuItem";
+            this.설정ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.설정ToolStripMenuItem.Text = "설정";
+            // 
+            // FontSetting
+            // 
+            this.FontSetting.Name = "FontSetting";
+            this.FontSetting.Size = new System.Drawing.Size(180, 22);
+            this.FontSetting.Text = "글꼴";
+            this.FontSetting.Click += new System.EventHandler(this.FontSetting_Click);
+            // 
+            // Info
+            // 
+            this.Info.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Infomation});
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(43, 20);
+            this.Info.Text = "정보";
+            // 
+            // Infomation
+            // 
+            this.Infomation.Name = "Infomation";
+            this.Infomation.Size = new System.Drawing.Size(180, 22);
+            this.Infomation.Text = "정보";
+            this.Infomation.Click += new System.EventHandler(this.Infomation_Click);
+            // 
+            // Notepad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Notepad";
+            this.Text = "Notepad";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Notepad_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileOpen;
+        private System.Windows.Forms.ToolStripMenuItem Save;
+        private System.Windows.Forms.ToolStripMenuItem SaveAs;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
+        private System.Windows.Forms.ToolStripMenuItem 편집ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Copy;
+        private System.Windows.Forms.ToolStripMenuItem Cut;
+        private System.Windows.Forms.ToolStripMenuItem Paste;
+        private System.Windows.Forms.ToolStripMenuItem 설정ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FontSetting;
+        private System.Windows.Forms.ToolStripMenuItem Info;
+        private System.Windows.Forms.ToolStripMenuItem Infomation;
+        private System.Windows.Forms.ToolStripMenuItem NewFile;
+    }
+}
---
+++ Notepad/Notepad.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Notepad : Form
+    {
+        private string path = "";       //path가 ""인 경우 새로 만들어지거나 처음인 경우
+        private enum State { StandBy, Writing };
+        private State state;
+        private bool isChanged = false;
+        private bool isLoaded = false;
+
+        public Notepad()
+        {
+            InitializeComponent();
+            state = State.Writing;
+            Text = "제목 없음";
+            path = "";
+            isChanged = false;
+            isLoaded = false;
+        }
+
+        private void SaveCheck(object sender, EventArgs e)
+        {
+            if(isChanged)
+            {
+                var result = MessageBox.Show("변경된 내용을 저장하시겠습니까?", "알림", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    if (path == "")//파일이 열린 경우가 아니라면
+                        SaveAs_Click(sender, e);
+                    else
+                        Save_Click(sender, e);
+                }
+            }
+        }
+
+        private void NewFile_Click(object sender, EventArgs e)
+        {
+            SaveCheck(sender, e);
+
+            Text = "제목 없음";
+            TextBox.Text = "";
+            path = "";
+
+            state = State.Writing;
+            isChanged = false;
+
+            isLoaded = true;
+        }
+
+        private void FileOpen_Click(object sender, EventArgs e)
+        {
+            SaveCheck(sender, e);
+
+            try
+            {
+                using (OpenFileDialog fileDialog = new OpenFileDialog()
+                {
+                    Title = "열기",
+                    Filter = "텍스트 파일 (*.txt)|*.txt|모든 파일 (*.*)|*.*"
+                })
+                {
+                    fileDialog.ShowDialog();
+                    path = fileDialog.FileName;
+                    Text = (System.IO.Path.GetFileName(path) == "") ? Text : System.IO.Path.GetFileName(path);
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+                throw;
+            }
+
+            if (path != "")
+            {
+                using (var reader = new System.IO.StreamReader(path))
+                {
+                    var file = reader.ReadToEnd();
+                    TextBox.Text = file;
+                }
+
+                state = State.StandBy;
+                isChanged = false;
+            }
+        }
+
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (!isChanged) return;
+
+            if (path == "")
+            {
+                SaveAs_Click(sender, e);
+                return;
+            }
+
+            using (var writer = new System.IO.StreamWriter(path))
+            {
+                writer.Write(TextBox.Text);
+            }
+
+            state = State.Writing;
+            Text = Text.Substring(0, Text.Length - 1);
+            isChanged = false;
+        }
+
+        private void SaveAs_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog fileDialog = new SaveFileDialog()
+            {
+                Title = "다른 이름으로 저장",
+                Filter = "텍스트 파일 (*.txt)|*.txt|모든 파일 (*.*)|*.*"
+            })
+            {
+                fileDialog.ShowDialog();
+                path = fileDialog.FileName;
+            }
+
+            if (path != "")
+            {
+                Text = System.IO.Path.GetFileName(path);
+
+                using (var writer = new System.IO.StreamWriter(path))
+                {
+                    writer.Write(TextBox.Text);
+                }
+
+                if (state == State.Writing)
+                {
+                    state = State.StandBy;
+                    isChanged = false;
+                }
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            SaveCheck(sender, e);
+            Application.Exit();
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            if(!isLoaded)
+            {
+                state = State.Writing;
+                if (state == State.Writing)
+                {
+                    if (!isChanged)
+                    {
+                        Text += "*";
+                        isChanged = true;
+                    }
+                }
+            }
+
+            isLoaded = false;
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(TextBox.SelectedText);
+            TextBox.Select(TextBox.TextLength, 1);
+        }
+
+        private void Cut_Click(object sender, EventArgs e)
+        {
+            string strCopy = TextBox.Text;
+            int startIndex = TextBox.SelectionStart;
+            int length = TextBox.SelectedText.Length;
+
+            Clipboard.SetText(TextBox.SelectedText);
+            TextBox.Text = strCopy.Substring(0, startIndex);
+            TextBox.Text += strCopy.Substring(startIndex + length);
+            TextBox.Select(TextBox.TextLength, 1);
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            TextBox.Text = TextBox.Text.Insert(TextBox.SelectionStart, Clipboard.GetText());
+            TextBox.Select(TextBox.TextLength, 1);
+        }
+
+        private void FontSetting_Click(object sender, EventArgs e)
+        {
+            using (FontDialog fontDialog = new FontDialog())
+            {
+                if (DialogResult.OK == fontDialog.ShowDialog())
+                {
+                    TextBox.Font = fontDialog.Font;
+                }
+            }
+        }
+
+        private void Notepad_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveCheck(sender, e);
+        }
+
+        private void Infomation_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kiheyunkim@gmail.com\n제작자:김기현", "정보", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
